48cfe5b75dc9803be9a410c9d98419ce06758e9b
karoliys <kzhibul24.11.2002@yandex.com>
2022-09-27 14:45:59 +0300

    Added modells for car and car_type

--- backend/car_rental_site/settings.py
+++ backend/car_rental_site/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-9o*znpjpl)h=2s#bj3$^a485pmnz2cr*d+zt01z6td6l(krww1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -83,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'mysql',
 		'USER': 'root',
-		'PASSWORD': 'matua535',
+		'PASSWORD': 'Tel2525787!@',
 		'HOST': 'db',
 		'PORT': '3306'
     }
@@ -130,4 +131,7 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
