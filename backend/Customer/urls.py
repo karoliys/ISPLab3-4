d290daaa8b6dd2f36605a92e40f0ddfb8c6ea30d
karoliys <kzhibul24.11.2002@yandex.com>
2022-09-27 15:18:14 +0300

    New Changes

--- backend/Customer/urls.py
+++ backend/Customer/urls.py
@@ -2,8 +2,15 @@ from django.urls import path, include
 from customer import views
 
 urlpatterns = [
-    path('index_user/', views.index_user,name="Index"),
-    path('sign_in/',views.sign_in,name="SignIn"),
-    path('log_out/',views.log_out,name="Logout"),    
-    path('registration/',views.registration,name="Register"),
+    path('', views.IndexUser.as_view(), name="IndexUser"),
+    path('about_site/', views.about_site, name="AboutSite"),
+    path('home/', views.Home.as_view(), name="Home"),
+    path('show_car/<slug:post_slug>/', views.ShowCar.as_view(), name="ShowCar"), 
+    path('sign_in/',views.SignIn.as_view(), name="SignIn"),
+    path('logout_customer/',views.logout_customer,name="LogoutCustomer"),    
+    path('registration/',views.Registration.as_view(), name="Registration"),
+    # path('search_car/', views.ChooseCar.as_view(), name='ChooseCar'),
+    # path('logined_customer/', views.LoginedCustomer.as_view(),name="LoginedCustomer"),
+    # path('registrated_customer/', views.registrated_customer,name="RegistratedCustomer"),
+    path('customer_profile/', views.customer_profile,name="CustomerProfile")
 ]
